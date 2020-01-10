d5c6bf35ce33b63934866a013df05da343492e1d
Glenn Y. Rolland <glenux@glenux.net>
2020-01-10 11:23:30 +0100

    Initial import

---
+++ provisioning/services.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+set -u # check undefined variables
+set -e # check errors
+
+# ici je mettrai de quoi installer automatiquement les différents logiciels
+# ...
+# Je configure mon réseau 
+cat > /etc/network/interfaces.d/eth1 <<-MARK
+allow-hotplug eth1
+iface eth1 inet static
+	address 10.0.3.10
+	netmask 255.255.255.0
+	gateway 10.0.3.1
+	pre-up ip route del default 
+MARK
+
+# J'applique ma configuration réseau
+ifdown eth1 || true
+ifup eth1
+
+DEBIAN_FRONTEND=noninteractive
+apt-get update
+apt-get install -y nginx
+
+echo "SUCCESS"
+
